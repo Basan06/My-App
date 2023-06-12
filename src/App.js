1d7824c0af3926248333987379acfa1c4ab47e61
Basan06 <vbbasanagouda@gmail.com>
2023-06-12 14:37:22 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [address, setAddress] = useState('');
+  const [bioData, setBioData] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBioData = {
+      name,
+      age,
+      address
+    };
+    setBioData([...bioData, newBioData]);
+    setName('');
+    setAge('');
+    setAddress('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bio Data Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Age:
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Address:
+          <textarea
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          ></textarea>
+        </label>
+        <br />
+        <button type="submit">Add Bio Data</button>
+      </form>
+
+      <h2>Bio Data List</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bioData.map((data, index) => (
+            <tr key={index}>
+              <td>{data.name}</td>
+              <td>{data.age}</td>
+              <td>{data.address}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
